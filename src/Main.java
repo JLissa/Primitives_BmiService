0f0b265786e08149a4d904cce79e8eab7de75590
Julia Hlimoneko <707010@gmail.com>
2022-12-22 16:54:58 +0200

    Changing Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 60;
-        double height = 1.70;
-        double bmi = service.calculate(weight, height);
+//        int weight = 60;
+//        double height = 1.70;
+//        double bmi = service.calculate(weight, height);
+        double bmi = service.calculate(60, 1.70);
+        System.out.println("Ваш индекс массы: " + bmi);
+
+        bmi = service.calculate(50, 1.80);
+        System.out.println("Ваш индекс массы: " + bmi);
+
+        bmi = service.calculate(100, 1.90);
         System.out.println("Ваш индекс массы: " + bmi);
     }
 }
